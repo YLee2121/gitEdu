8bdc27bc12d97cd57f0f7897684af880fc47f914
Yi Lee <87312700+YLee2121@users.noreply.github.com>
2023-02-23 17:03:23 -0500

    main cal app work flow

--- calculator/main.py
+++ calculator/main.py
@@ -1,7 +1,31 @@
 from .operator import adition, minus, multiply, divide
 
-def main():
-    print("hello world!")
+STOP = 'stop'
+ADD = '+'
+MINUS = '-'
+MULTIPLY = '*'
+DIVIDE = '/'
 
-if __name__ == '__main__':
-    main()
+
+def main():
+
+    while True:
+        
+        print('stop for exit')
+        num1 = input('Enter first number: ')
+        if num1 == STOP:
+            break
+        num2 = input('Enter second number: ')
+        operator = input('Enter operator: ')
+
+        if operator == ADD:
+            print(adition(num1, num2))
+        elif operator == MINUS:
+            print(minus(num1, num2))
+        elif operator == MULTIPLY:
+            print(multiply(num1, num2))
+        elif operator == DIVIDE:
+            print(divide(num1, num2))
+        
+
+        
