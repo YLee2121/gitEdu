2ba250c4ae1401481bbb8b3c9dffc99d3037aeaf
I-Wei (Claire) Chen <88813371+cheniwei@users.noreply.github.com>
2023-02-27 17:59:08 -0800

    Update operator.py

--- calculator/operator.py
+++ calculator/operator.py
@@ -1,11 +1,11 @@
 def adition(num1: str, num2: str) -> str:
-    pass
+    return str(float(num1) + float(num2))
 
 def minus(num1: str, num2: str) -> str:
-    pass
+    return str(float(num1) - float(num2))
 
 def multiply(num1: str, num2: str) -> str:
-    pass
+    return str(float(num1) * float(num2))
 
 def divide(num1: str, num2: str) -> str:
-    pass
+    return str(float(num1) / float(num2))
